cad2ff89010fac0e3ea5243e9159bb5c71be8f17
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2021-11-04 19:20:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+detached-shell-dot-net-core
---
+++ detached-shell-dot-net-core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace detached_shell_dot_net_core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
