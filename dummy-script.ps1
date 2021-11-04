d30db11261f3e819876c83237b5e4d39f1a627aa
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2021-11-04 19:20:43 +0100

    feat: dummy scripts

---
+++ dummy-script.ps1
@@ -0,0 +1,18 @@
+﻿# Just basic powershell script that prints some output and wait between prints
+Write-Host 'Hello, World!'
+
+Start-Sleep -s 1
+
+Write-Host 'successfull'
+
+Start-Sleep -s 1
+
+Write-Host 'Hello, World!'
+
+Start-Sleep -s 1
+
+Write-Host 'Hello, World!'
+
+Start-Sleep -s 1
+
+Write-Host 'Hello, World!'
---
+++ dummy-script.sh
@@ -0,0 +1,18 @@
+﻿# Just basic shell script that prints some output and wait between prints
+echo 'Hello, World!'
+
+sleep 1
+
+echo 'successfull'
+
+sleep 1
+
+echo 'Hello, World!'
+
+sleep 1
+
+echo 'Hello, World!'
+
+sleep 1
+
+echo 'Hello, World!'
